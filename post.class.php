85638d310971dd417ef5dd7d8b21bf7a290404e6
Serhat YAREN <64776783+SerhatYaren09@users.noreply.github.com>
2021-09-10 14:17:44 +0300

    Post.class son hali

--- post.class.php
+++ post.class.php
@@ -8,140 +8,196 @@
  //  Yazıyı güncelleme işlemlerinin yapılması
  //  Yazıyı silme işlemlerinin yapılması
 
+//manage.php - post listesi görünüyor mu? Yeni yazı eklemek için bir link bulunuyor mu? Yazı listesinde herbir yazı için düzenleme ve silme linkleri bulunuyor mu?
+//manage.php?action=create - yeni post oluşturma formu görüntüleniyor mu?(YAPILDI)
+//manage.php veya manage.php?action=store - Yeni post oluşturma işlemi yapıldı mı?(YAPILDI)
+//manage.php?action=edit&post=1 - İlgili postu güncelleme için form görüntüleniyor mu? Formdaki alanların değerleri ilgili postun değerleri ile güncellenmiş mi?(YAPILDI)
+//manage.php veya manage.php?action=update&post=1 - İlgili postun güncelleme işlemi yapıldı mı? (YAPILDI)
+//manage.php?action=delete&post=1 - İlgili postun silinme işlemi yapıldı mı? (YAPILDI)
+
 //dosya yolu bulunamassa eğer fatal error verdiğinden require_once tercih ediyorum
 requre_once "db.class.php";
 
 class Post extends Db
 {
-
-    public function getPostlist()
+   private $posts = [];
+   private $manage = 0;
+   $action = 0;
+    public function getPostlist(int $manage = 0)
+    { // db den post dataları pull etme 
+        $this->manage = $manage;
+        $this->posts = $this->getPostDetails();
+        switch ($action) 
+        {
+          case "Create":
+                $this->createForm();
+                break;
+          case "Store":
+                $this->createFormPost();
+                break;
+          default:
+               $this-->listPost();
+        }
+    }
+       public function getSelectPostList(int $manage = 0)
     {
-        $sql = "SELECT * FROM posts";
-        $explanation = $this->connection()->extend($sql); 
-        $explanation->execute();
+        $id = $_GET["post"];
+        $action = 0;
+        if (isset($_GET["action"]))
+        {
+            $action = $_GET["action"];
+        }
+        //Db den Id to post value pull 
+        $this->manage = $manage; 
+        $this->posts = $this->getSelectPostDetails($id);
+        switch ($action) 
+        {
+           case "Create":
+                $this->createForm();
+                break;
+           case "Store":
+                $this->createFormPost();
+                break;
+           case "Edit":
+                $this->editForm($id);
+                break;
+           case "Update":
+                $this->editFormPost($id);
+                break;
+           case "Delete":
+                $this->DeletePost($id);
+                break;
+            default:
+                echo "Undefined";
+                $this->listPost();
+        }
+    }
+  
+    public function formPost()
+    {
+        if (!empty($_POST["title"])) 
+        {
+            $title = $_POST["title"];
+            $content = $_POST["content"];
+            $this->addPost($title, $content);
+        }
+    }
+    public function createForm()
+    {
+        $action = "manage.php?action=store";?>
+        <form method="post" action="<?php echo $action; ?>"> //Yeni post oluşturma işlemi yapılması
+           <div>
+           <label for="exampleFormControlInput">Title</label>
+           <input type="text" name="title" required value>
+            </div>              
+              <div>
+              <label for="exampleFormControlTextarea">Content</label>
+              <textarea name="content"  required value rows="6">
+               </textarea>
+               </div>
+       <button type="submit">Send</button>
+         </form>
+  <?php
+    }
+      public function createFormPost()
+    {
+        if (!empty($_POST["title"])) 
+        {
+            $title = $_POST["title"];
+            $content = $_POST["content"];
+            $this->addPost($title, $content);
+            echo 'Ekleme işlemi gerçekleşti . <a href="manage.php">Geri</a>';
+        } 
+        else 
+        {
+            echo "ERROR";
+        }
+    }
+  
+   public function listPost()
+    { 
+        $manage = $this->manage;
+        $posts = $this->posts;
+       
+        if(array($posts)) 
+      {
+           if(count($posts) == 1) 
+        {
+
+          if(!empty($posts[0]["Id"])) //Seçilen Post kısmı
+            {
+               $this->Post($posts[0]["Id"], $posts[0]);
+            }
+         }   
+            else 
+            {
+                echo "POST YOK";
+            }
+        }
+    }
+  
+      public function postView(int $a, array $b) // Postları görüntüleme
+    {
+        $manage = $this->manage;
+        $posts = $this->posts;
         
-    $result = $explanation->fetchAll();
+    <?php if ($manage) { ?>
+    <a href="manage.php?action=delete&post=<?php echo $a; ?>" role="button">Delete</a>  
+    <a href="manage.php?action=edit&post=<?php echo $a; ?>" role="button">Edit</a>
+    <?php }
+     <a href="<?php echo !empty($this->manage)
+      ? "manage.php"
+      : "index.php"; ?>?post=<?php echo $a; ?>" role="button">Show</a> 
+  <?php } ?>
+
+  <?php}
+    public function editForm($id = 0)
+    {
+        if (!empty($id))
+         {
+            $b = $this->getSelectPostDetails($id);
+            foreach($b as $a => $c)
+            {
+                $title = $c["Title"];
+                $content = $c["Content"];
+            }
+        
+            $action = "manage.php?action=update&post=$id";
+
+        }
+        ?>
+    <form method="post" action="<?php echo $action; ?>">
+     <div>
+    <label for="exampleFormControlInput">Title</label>
+    <input type="text" name="title" required value="<?php echo $title; ?>">                
+     </div>
     
-        foreach ($result as $s)
-        {
-          echo $s[1] . "<br>";
-          echo $s[2] . "<br>";
-          echo $s[3] . "<br>";
-        }
+  <div>
+  <label for="exampleFormControlTextarea1">Content</label>
+  <textarea name="content" required value rows="6"><?php echo $content; ?></textarea>
+  </div>
+  <button type="submit">Send</button>
+    </form>
+  <?php
     }
 
-    public function getparticularPost()
+      public function editFormPost($id)
     {
-     $id = $_GET["id"]?? null;
-        if (!$id)
+        if (!empty($_POST["title"])) 
         {
-          header("index.php git");
-        }
-          $sql = "SELECT * FROM posts WHERE id = _id";
-          $explanation = $this->connection()->extend($sql);
-          $explanation->Value("_id",$id);
-          $explanation->execute();
-
-        while($result = $explanation->fetchAll()){
-            return $result;
+            $title = $_POST["title"];
+            $content = $_POST["content"];
+            $this->updatePost($id, $title, $content);
+            echo 'Update işlemi gerçekleştirildi. <a href="manage.php">Geri</a>';
         }
     }
 
-    public function createPost(){
-        $error = [];
-        $title = $_POST["title"]?? null;
-        $content = $_POST["content"]?? null;
-
-        if (!$title) //Başlık yoksa 
-        {
-            $error[] = "Başlık gerekli"; //Uyarı ver
-        }
-        if (!$content) //İçerik yoksa
-        {
-            $error[] = "İçerik gerekli"; //uyarı ver
-        }
-
-        if (!empty($error)) //$error boş değilse
-        {
-            <?php foreach ($error as $er):?> //$error daki içeriği $er aktar 
-            <div><?php echo $er ?> </div> //$er'i ekrana bas
-            <?php endforeach; ?>
-            </div> <?php
-        }
-
-        else 
-        {  // Yeni yazı ekleme işlemlerinin yapılması
-            
-            $sql="INSERT INTO sections(title, content) 
-                  VALUES (_title, _content)";
-            $explanation = $this->connection()->extend($sql);
-            $explanation->Value('_title',$title);
-            $explanation ->Value('_content',$content);
-            $explanation->execute();
-
-            header("index.php git");
-        }
-    }
-  
- //  Yazıyı güncelleme işlemlerinin yapılması
- 
-   public function updatePost()
+ public function DeletePost(int $id)
     {
-        $id = $_GET["id"]?? null;
-
-    if (!$id)
-        {
-          header("index.php git");
-        }
-
-        $error = [];
-        $title = $_POST["title"]?? null;
-        $content = $_POST["content"]?? null;
-
-        if(!$title)
-        {
-          $error[] = 'Başlık gerekli';
-        }
-        if (!$content)
-        {
-            $error[] = "İçerik gerekli";
-        }
-
-     if (!empty($error)) //$error boş değilse
-        {
-            <?php foreach ($error as $er):?> //$error daki içeriği $er aktar 
-            <div><?php echo $er ?> </div> //$er'i ekrana bas
-            <?php endforeach; ?>
-            </div> <?php
-        }
-
-        else 
-        {
-            $sql = "UPDATE sections SET title = _title, content = _content WHERE id = _id)";
-            $explanation = $this->connection()->prepare($sql);
-            $explanation->Value("_title" , $title);
-            $explanation->Value("_content" , $content);
-            $explanation->Value("_id" , $id);
-            $explanation->execute();
-
-            header("index.php git");
-        }
+        $this->deletePost($id);
+        echo 'Delete işlemi gerçekleştirildi. <a href="manage.php">Geri</a>';
     }
-//  Yazıyı silme işlemlerinin yapılması
-  
- public function deletePost()
-    {
-    $id = $_GET["id"]?? null;
-    if(!$id)
-    {
-      header ("index.php git");
-    }
-     $sql = "DELETE FROM posts WHERE id = _id";
-     $explanation = $this->connection()->extend($sql);
-     $explanation->Value("_id",$id);
-     $explanation->execute();
-      header("index.php git");
-    }
+
 }
 
 ?>
