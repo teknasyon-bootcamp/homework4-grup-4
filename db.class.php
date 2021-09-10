3b7a985c41ff68c2af70e48fa8fe5d1d60a16493
Serhat YAREN <64776783+SerhatYaren09@users.noreply.github.com>
2021-09-10 10:10:26 +0300

    update db.class

--- db.class.php
+++ db.class.php
@@ -21,6 +21,53 @@ class Db
         }
         
     }
+        protected function addPost(string $title, string $content)
+    {
+        try 
+        {
+          $sql = "INSERT INTO posts (Title, Content) 
+                VALUES (_Title, _Content)";
+            $explanation = $this->connection()->extend($sql);
+            $explanation->Value("_Title", $title);
+            $explanation->Value("_Content", $content);
+            $explanation->execute();
+        } 
+        catch (Exception $e) 
+        {
+            echo "Db hatası: " . $e->getMessage();
+        }
+    }
+
+    protected function updatePost( int $id,string $title,string $content) 
+    {
+        try 
+        {
+          $sql ="UPDATE posts SET Title = _Title, Content = _Content WHERE Id = :Id";
+            $explanation = $this->connection()->extend($sql);
+            $explanation->Value("_Title", $title);
+            $explanation->Value("_Content", $content);
+            $explanation->Value("_Id", $id);
+            $explanation->execute();
+        } 
+        catch (Exception $e) 
+        {
+            echo "Db hatası: " . $e->getMessage();
+        }
+    }
+    protected function deletePost(int $id)
+    {  
+        try 
+        {
+            $sql = "DELETE FROM posts WHERE Id = _Id";
+            $explanation = $this->connection()->extend($sql);
+            $explanation->bindValue("_Id", $id);
+            $explanation->execute();
+        } 
+        catch (Exception $e) 
+        {
+            echo "Db hatası: " . $e->getMessage();
+        }
+    }
 }
 ?>
 
